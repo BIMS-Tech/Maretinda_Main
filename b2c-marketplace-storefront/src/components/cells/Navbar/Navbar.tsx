5792be93abb08a6affe306b6ae5239502604b5d2
Dennis Abanto <dennis@cynder.io>
2025-10-17 08:51:45 +0800

    fix: search bar padding

--- b2c-marketplace-storefront/src/components/cells/Navbar/Navbar.tsx
+++ b2c-marketplace-storefront/src/components/cells/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar = ({
 	categories: HttpTypes.StoreProductCategory[];
 }) => {
 	return (
-		<div className="relative flex flex-col md:border-b justify-between items-stretch lg:items-center px-6 gap-5">
+		<div className="relative flex flex-col md:border-b justify-between items-stretch lg:items-center px-4 lg:px-6 gap-5">
 			<div className="flex lg:hidden items-center">
 				<NavbarSearch />
 			</div>
