fd5512c4218a24626c3d4eb85e858050ddf37463
Tamim Hossain <amdtamim3@gmail.com>
2025-11-05 20:50:26 +0600

    fixing lint errors and index logo name errors

--- b2c-marketplace-storefront/next.config.ts
+++ b2c-marketplace-storefront/next.config.ts
@@ -32,6 +32,10 @@ const nextConfig: NextConfig = {
 				hostname: 's3.eu-central-1.amazonaws.com',
 				protocol: 'https',
 			},
+			{
+				hostname: 's3.ap-southeast-1.amazonaws.com',
+				protocol: 'https',
+			},
 		],
 	},
 	logging: {
