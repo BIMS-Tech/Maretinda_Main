478bbb839fac033cc2390e38852c067539699127
Marco Padillo <marco@cynder.io>
2025-11-06 16:38:53 +0800

    fix: sort categories by rank

--- b2c-marketplace-storefront/src/components/molecules/CategoryNavbar/CategoryNavbar.tsx
+++ b2c-marketplace-storefront/src/components/molecules/CategoryNavbar/CategoryNavbar.tsx
@@ -4,6 +4,7 @@ import type { HttpTypes } from '@medusajs/types';
 import { useParams } from 'next/navigation';
 
 import { CategoryNavDropdown } from '@/components/cells/CategoryNavDropdown/CategoryNavDropdown';
+import { sortCategories } from '@/lib/utils';
 
 export const CategoryNavbar = ({
 	categories,
@@ -16,17 +17,19 @@ export const CategoryNavbar = ({
 
 	return (
 		<nav className="flex md:items-center flex-col md:flex-row">
-			{categories?.map(({ category_children, id, handle, name }) => (
-				<CategoryNavDropdown
-					handle={handle}
-					handleCategory={handle === category}
-					id={id}
-					key={id}
-					name={name}
-					onClick={() => (onClose ? onClose(false) : null)}
-					subcategories={category_children}
-				/>
-			))}
+			{categories
+				?.sort(sortCategories)
+				.map(({ category_children, id, handle, name }) => (
+					<CategoryNavDropdown
+						handle={handle}
+						handleCategory={handle === category}
+						id={id}
+						key={id}
+						name={name}
+						onClick={() => (onClose ? onClose(false) : null)}
+						subcategories={category_children}
+					/>
+				))}
 		</nav>
 	);
 };
