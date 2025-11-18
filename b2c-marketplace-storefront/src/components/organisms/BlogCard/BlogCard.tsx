34f22d4ca97c66e70af73940358749b99b4ccf96
Dennis Abanto <dennis@cynder.io>
2025-11-18 15:47:49 +0800

    feat: change blog card ui

--- b2c-marketplace-storefront/src/components/organisms/BlogCard/BlogCard.tsx
+++ b2c-marketplace-storefront/src/components/organisms/BlogCard/BlogCard.tsx
@@ -1,5 +1,3 @@
-import { ArrowLongRight } from '@medusajs/icons';
-import { Avatar } from '@medusajs/ui';
 import Image from 'next/image';
 
 import { Button } from '@/components/atoms';
@@ -26,26 +24,17 @@ export function BlogCard({ post }: BlogCardProps) {
 					width={400}
 				/>
 			</div>
-			<div className="flex flex-col space-y-2 items-start justify-end p-4 bg-gradient-to-t from-black/90 to-white/5 text-tertiary absolute bottom-0 left-0 rounded-b-xs w-full h-full">
+			<div className="flex flex-col space-y-1 items-start justify-end p-4 bg-gradient-to-t from-black/90 to-white/5 text-tertiary absolute bottom-0 left-0 rounded-b-xs w-full h-full">
 				<span className="text-sm !font-bold">{post.category}</span>
 				<h3 className="heading-md font-lora !font-bold">
 					{post.title}
 				</h3>
-				<div className="flex items-center justify-start gap-2.5">
-					<Avatar
-						className="w-7"
-						fallback="M"
-						size="small"
-						src="https://avatars.githubusercontent.com/u/10656202?v=4"
-					/>
-					<span className="text-md !font-normal">
-						{post.author}, {post.date}
-					</span>
-				</div>
+				<span className="text-md !font-normal">
+					{post.author}, {post.date}
+				</span>
 				{/* <p className="text-md line-clamp-2">{post.excerpt}</p> */}
-				<Button className="!mt-4 min-w-[12px] !font-medium label-md text-white px-3.5 py-2 bg-transparent hover:bg-action  hover:border-action gap-1.5 flex items-center rounded-[6px] transition-colors border border-white">
-					Read More
-					<ArrowLongRight width={20} />
+				<Button className="hidden group-hover:flex !mt-2.5 min-w-[12px] !font-medium label-md text-white px-0 py-0 bg-transparent hover:bg-transparent underline underline-offset-4 items-center">
+					Read More...
 				</Button>
 			</div>
 		</LocalizedClientLink>
