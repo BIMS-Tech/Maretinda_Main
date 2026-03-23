2644077d0b79fcc905e222c1461d8925b3572327
Tamim Hossain <amdtamim3@gmail.com>
2026-03-24 02:26:46 +0600

    Updated sell now button

--- maretinda-store/src/components/cells/SellNowButton/SellNowButton.tsx
+++ maretinda-store/src/components/cells/SellNowButton/SellNowButton.tsx
@@ -4,13 +4,7 @@ import { Button } from '@/components/atoms';
 
 export const SellNowButton = () => {
 	return (
-		<Link
-			href={
-				process.env.NEXT_PUBLIC_ALGOLIA_ID === 'UO3C5Y8NHX'
-					? 'https://vendor-sandbox.vercel.app/'
-					: 'https://vendor.mercurjs.com'
-			}
-		>
+		<Link href="https://testvendor.maretinda.com">
 			<Button className="ml-1 min-w-[74px] lg:min-w-[100px] group !font-medium text-md px-4 py:2 lg:!py-2.5 lg:px-5 flex items-center rounded-xs lg:rounded-sm">
 				Sell Now
 			</Button>
